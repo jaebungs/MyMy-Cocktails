9d820592abffaa03b4e1bb54bd506e8504542878
jaebungs <jaebungs@gmail.com>
2021-01-21 11:13:45 -0500

    sign up page with material ui. no styling yet

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -1,13 +1,101 @@
-import React from 'react';
+import React, { useState } from "react";
+import {
+  Container,
+  Grid,
+  Typography,
+  Paper,
+  Avatar,
+  Button,
+  TextField,
+} from "@material-ui/core";
+import LockRoundedIcon from "@material-ui/icons/LockRounded";
+import Input from "./Input";
 
 const Auth = () => {
-    const state = null;
-    
-    return (
-        <div>
-            <h1>Sign In Page</h1>
-        </div>
-    )
-}
+  const [ isSignUp, setIsSignUp ] = useState(false);
+  const state = null;
+
+  const handleSubmit = () => {};
+  const handleChange = () => {};
+  const switchSignMode = () => {
+    setIsSignUp((prev)=> !prev)
+  }
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Paper elevation={3}>
+        <Avatar>
+          <LockRoundedIcon />
+        </Avatar>
+        <Typography variant="h6">{isSignUp ? "Sign Up" : "Sign In"}</Typography>
+        <form onSubmit={handleSubmit}>
+          <Grid container spacing={3}>
+            {isSignUp && (
+              <div>
+                <Input
+                  name="firstName"
+                  size="6"
+                  type="text"
+                  placeholder="First Name"
+                  handleChange={handleChange}
+                  autoFocus
+                />
+                <Input
+                  name="lastName"
+                  size="6"
+                  type="text"
+                  placeholder="Last Name"
+                  handleChange={handleChange}
+                />
+              </div>
+            )}
+            <Input
+              name="email"
+              type="email"
+              placeholder="Enter Email"
+              handleChange={handleChange}
+              autoFocus
+            />
+            <Input
+              name="password"
+              type="password"
+              placeholder="Enter Passowrd"
+              handleChange={handleChange}
+            />
+            {isSignUp && (
+              <Input
+                name="confirmPassword"
+                type="password"
+                placeholder="Confirm Passowrd"
+                handleChange={handleChange}
+              />
+            )}
+          </Grid>
+          <Button type="Submit" color="primary" variant="contained" fullWidth >{isSignUp ? 'Sign Up' : 'Sign In'}</Button>
+        </form>
+        {!isSignUp ? (
+          <div>
+            <p>Don't have an account?</p>
+            <Button
+              onClick={switchSignMode}
+            >
+              Create an account here
+            </Button>
+          </div>
+        ) : (
+            <div>
+            <p>I already have an account.</p>
+
+                <Button
+              onClick={switchSignMode}
+            >
+              Go to Sign In
+            </Button>
+            </div>
+        )}
+      </Paper>
+    </Container>
+  );
+};
 
 export default Auth;
---
+++ client/src/components/Input.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Grid, TextField} from '@material-ui/core';
+
+// Custom components for Input
+const Input = ({name, size, type, label, placeholder, handleChange, autoFocus }) => {
+    return (
+        <Grid item xs={12} sm={size ? parseInt(size) : 12}>
+            <TextField 
+                name={name}
+                type={type}
+                labe={label}
+                placeholder={placeholder}
+                onChange={handleChange}
+                autoFocus={autoFocus}
+                variant="outlined"
+                required
+                fullWidth
+            />
+        </Grid>
+    )
+}
+
+export default Input;
