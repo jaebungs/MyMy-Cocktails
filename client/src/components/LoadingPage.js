b744a444fd24a60cb93aca24443deb43a26d6679
jaebungs <jaebungs@gmail.com>
2021-02-15 15:57:18 -0500

    add animation on loading page

--- client/src/components/LoadingPage.js
+++ client/src/components/LoadingPage.js
@@ -3,15 +3,21 @@ import './styles/loadingStyles.css';
 
 const LoadingPage = () => {
   return (
-    <div className='container'>
-      <p>L</p>
-      <p>O</p>
-      <p>A</p>
-      <p>D</p>
-      <p>I</p>
-      <p>N</p>
-      <p>G</p>
-    </div>
+    <div className="loading-container">
+  <div className="flex-column">
+    <div className="circle"></div>
+    <div className="shadow"></div>
+  </div>
+  <div className="flex-column">
+    <div className="circle"></div>
+    <div className="shadow"></div>
+  </div>
+  <div className="flex-column">
+    <div className="circle"></div>
+    <div className="shadow"></div>
+  </div>
+  <p className="text">Loading...</p>
+</div>
   );
 };
 
